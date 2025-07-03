d13a1efc53bae25d7b1bb05c26a5718d8aaacd9b
work arfat <work.arfat@gmail.com>
2025-07-03 15:41:31 +0530

    sign up and sign in pages added

--- src/components/auth-model.tsx
+++
@@ -1,8 +0,0 @@
-
-function AuthModel() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,30 @@
 import { NotepadText } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-      <h1 className="flex items-center gap-2">
-        <NotepadText className="w-8 h-8 text-[#5C6AC4]" />
-        <span className="text-3xl font-semibold">LetterGenie</span>
-      </h1>
+      <Link href="/">
+        <h1 className="flex items-center gap-2">
+          <NotepadText className="w-8 h-8 text-[#5C6AC4]" />
+          <span className="text-3xl font-semibold">LetterGenie</span>
+        </h1>
+      </Link>
       <ul>
         <li className="inline-block mr-4">
-          <Button className="lg:text-lg cursor-pointer text-white bg-[#5C6AC4]">
-            Sign Up
-          </Button>
+          <Link href="/sign-up">
+            <Button className="lg:text-lg cursor-pointer text-white bg-[#5C6AC4]">
+              Sign Up
+            </Button>
+          </Link>
         </li>
         <li className="inline-block">
-          <Button variant="outline" className="lg:text-lg cursor-pointer">
-            Sign In
-          </Button>
+          <Link href="/sign-in">
+            <Button variant="outline" className="lg:text-lg cursor-pointer">
+              Sign In
+            </Button>
+          </Link>
         </li>
       </ul>
     </nav>
