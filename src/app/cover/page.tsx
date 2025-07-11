1cee208e7613ed5e9b1f1cf5e113aecbfe4108ac
work arfat <work.arfat@gmail.com>
2025-07-11 15:52:44 +0530

    profile has to be completed added in cover letter

--- src/app/cover/page.tsx
+++ src/app/cover/page.tsx
@@ -1,6 +1,11 @@
 import CoverLetter from "@/components/cover/cover-letter";
-import { getCoverLetterByClerkId } from "@/lib/queries";
+import { Button } from "@/components/ui/button";
+import {
+  checkIfUserProfileExists,
+  getCoverLetterByClerkId,
+} from "@/lib/queries";
 import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
@@ -8,5 +13,25 @@ export default async function Page() {
   if (!userId) redirect("/sign-in");
 
   const coverLetter = await getCoverLetterByClerkId(userId);
-  return <CoverLetter coverLetter={coverLetter} />;
+  const userProfileExists = await checkIfUserProfileExists();
+
+  return (
+    <>
+      {userProfileExists ? (
+        <CoverLetter coverLetter={coverLetter} />
+      ) : (
+        <section className="min-h-screen container max-w-5xl mx-auto text-center pt-20 space-y-4">
+          <h2 className="text-2xl font-semibold">Your Profile is Incomplete</h2>
+          <p className="lg:text-lg">
+            Please complete your profile to generate a cover letter.
+          </p>
+          <Link href="/profile">
+            <Button className="text-white bg-[#5C6AC4] hover:bg-[#4C5DB8] transition-colors duration-200 cursor-pointer">
+              Go to Profile
+            </Button>
+          </Link>
+        </section>
+      )}
+    </>
+  );
 }
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,5 +1,6 @@
 "use server";
 import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 
 export async function getProfileByEmail(email: string) {
   try {
@@ -52,6 +53,26 @@ export async function getCoverLetterByClerkId(clerkId: string) {
   } catch (error) {
     console.error("Error fetching cover letter:", error);
     return null;
+  }
+}
+
+export async function checkIfUserProfileExists(): Promise<boolean> {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+
+    const profile = await db.userProfile.findUnique({
+      where: { clerkId: userId },
+      select: { id: true },
+    });
+
+    return !!profile;
+  } catch (error) {
+    console.error("Error checking user profile existence:", error);
+    return false;
   }
 }
 
